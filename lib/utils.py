eb92fd8603203fcbf650c5e716a0b389960cb87b
mviano <matias.a.viano@gmail.com>
2023-03-28 12:22:51 -0300

    selection NVIDIA

--- lib/utils.py
+++ lib/utils.py
@@ -117,7 +117,7 @@ class _Backend():  # pylint:disable=too-few-public-methods
             txt = ", ".join([": ".join([key, val.upper().replace("_", " ")])
                              for key, val in self._backends.items()])
             #selection = input(f"{txt}: ")
-            selection = f"{txt}: 3"
+            selection = 3
             if selection not in self._backends:
                 print(f"'{selection}' is not a valid selection. Please try again")
                 continue
